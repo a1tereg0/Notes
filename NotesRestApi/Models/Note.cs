34fe2b6e22a476b542257ca3bc9a88e11cccb1fe
Prashant Dahal <prashantdah41@gmail.com>
2021-07-30 15:01:43 -0400

    Add Data annotations to note modal class

--- NotesRestApi/Models/Note.cs
+++ NotesRestApi/Models/Note.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,19 @@ namespace NotesRestApi.Models
 {
     public class Note
     {
-         public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(60, ErrorMessage = "Your title should exceed sixty characters")]
+        [Required]
         public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public DateTime LastModified { get; set; }
-        public bool IsFavourite { get; set; }
-        public bool IsTrashed { get; set; }
+        public bool IsFavourite { get; set; } = false;
+        public bool IsTrashed { get; set; } = false;
 
     }
 }

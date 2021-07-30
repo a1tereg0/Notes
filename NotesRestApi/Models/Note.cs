05651697e54411318d0dc2054e31f60b3cb784a3
Prashant Dahal <prashantdah41@gmail.com>
2021-07-30 14:19:44 -0400

    Add attributes for note model

--- NotesRestApi/Models/Note.cs
+++ NotesRestApi/Models/Note.cs
@@ -7,5 +7,12 @@ namespace NotesRestApi.Models
 {
     public class Note
     {
+         public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsFavourite { get; set; }
+        public bool IsTrashed { get; set; }
+
     }
 }

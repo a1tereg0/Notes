b1cf8cbdae8fc6237f0b7c189569f7c6782cfa06
Prashant Dahal <prashantdah41@gmail.com>
2021-07-30 15:20:10 -0400

    Add NoteDbContext as service on startup file

--- NotesRestApi/Context/NoteDbContext.cs
+++ NotesRestApi/Context/NoteDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NotesRestApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NotesRestApi.Context
 {
-    public class NoteDbContext
+    public class NoteDbContext: DbContext
     {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
     }
 }

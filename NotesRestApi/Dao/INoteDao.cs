8d978c14925f175c82e0629d3468e36ed4770b3a
Prashant Dahal <prashantdah41@gmail.com>
2021-07-30 16:24:32 -0400

    Add data access methods to the interface

--- NotesRestApi/Dao/INoteDao.cs
+++ NotesRestApi/Dao/INoteDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NotesRestApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace NotesRestApi.Dao
 {
     interface INoteDao
     {
+        public Note AddNote(Note note);
+        public void DeleteNote(int id);
+        public void UpdateNote(Note note);
+        public Note GetNote(int id);
+        public List<Note> GetAllNotes();
+
     }
 }

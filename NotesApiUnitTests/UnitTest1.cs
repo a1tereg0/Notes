82159bdb9aab02b3f92888d6110c6105d77070c5
prashant dahal <prashantdah41@gmail.com>
2021-08-19 18:51:36 -0400

    Create Unit Test project for api

---
+++ NotesApiUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotesRestApi.Controllers;
+
+namespace NotesApiUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+    }
+}
